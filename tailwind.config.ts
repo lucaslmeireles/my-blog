0bcb101164aa32736ad494d9f060bb8e843d1d0d
lucaslmeireles <86924068+lucaslmeireles@users.noreply.github.com>
2024-01-11 12:53:00 -0300

    Minor changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,8 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'light': 'url("./images/bg-main-light.jpg")',
-        'dark': 'url("./images/bg-night.jpg")'
+        'light': 'url("/images/bg-main-light.jpg")',
+        'dark': 'url("/images/bg-night.jpg")'
       },
       screens: {
         'xs': '375px',
